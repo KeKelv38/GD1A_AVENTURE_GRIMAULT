7fd9d3b0fc2ddfef10ae267db3ae0407c1191dd1
KeKelv38 <k.grimault@rns.ecolescreatives.com>
2024-03-20 11:43:23 +0100

    Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayeMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+
+    private Vector2 _movement;
+
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
+
+        _rb.velocity = _movement * _moveSpeed;
+    }
+
+}
