d268878e7f78104d3f0c39aa0110d48eacb45db2
KeKelv38 <k.grimault@rns.ecolescreatives.com>
2024-03-21 16:48:30 +0100

    Changement de scene fonctionnel

--- Assets/Scripts/Dontdestroy.cs
+++ Assets/Scripts/Dontdestroy.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dontdestroy : MonoBehaviour
+public class dontdestroy : MonoBehaviour
 {
-    void Start()
+    private void Start()
     {
         DontDestroyOnLoad(this);
     }
---
+++ Assets/Scripts/NextSceneScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class NextSceneScript : MonoBehaviour
+{
+    [SerializeField] bool toNextLevel;
+    [SerializeField] string levelName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (toNextLevel)
+            {
+                SceneController.instance.NextLevel();
+            }
+            else
+            {
+                SceneController.instance.LoadScene(levelName);
+            }
+        }
+
+    }
+}
