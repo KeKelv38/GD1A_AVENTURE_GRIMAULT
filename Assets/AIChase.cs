14f83d0f940f4aa7c87e17c3c46837bc7d763dfa
KeKelv38 <k.grimault@rns.ecolescreatives.com>
2024-04-08 17:22:01 +0200

    Ennemy qui suit le joueur avec animation

---
+++ Assets/AIChase.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIChase : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+    public float distanceBetween;
+    public Animator animator;
+
+
+    private float distance;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+
+        Vector3 movement = new Vector3(direction.x, direction.y, 0.0f) * speed;
+
+        animator.SetFloat("Horizontal", direction.x);
+        animator.SetFloat("Vertical", direction.y);
+        animator.SetFloat("Magnitude", direction.magnitude);
+
+
+        if (distance < distanceBetween)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            
+        }
+        else
+        {
+            animator.SetFloat("Magnitude", 0.0f);
+        }
+    }
+}
